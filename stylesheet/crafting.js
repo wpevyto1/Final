f28b6a604799819a6778057ee36f3c8c8cbde3fd
wpevyto1 <wpevyto1@my.westga.edu>
2023-11-29 14:57:54 -0500

    updated

--- stylesheet/crafting.js
+++ stylesheet/crafting.js
@@ -1,4 +1,22 @@
-document.getElementById('submit').addEventListener('click', validateInfo);
+
+document.addEventListener('DOMContentLoaded', function() {
+    populateStates();
+    toggleGiraffeInput();
+    toggleElephantInput();
+    toggleZebraInput();
+    toggleSlothInput();
+    toggleRacoonInput();
+
+    document.getElementById('price-button').addEventListener('click', function() {
+        if (validateInfo()) {
+            DisplayTotal();
+            document.getElementById('price-result').classList.remove('display-none');
+        } else {
+            console.log('Form data is invalid');
+        }
+    });
+});
+
 
 
 function validateInfo() {
@@ -50,4 +68,169 @@ function resetValidation() {
     for (i = 0; i < formControls.length; i++) {
         formControls[i].classList.remove('is-valid', 'is-invalid');
     }
+}
+
+
+
+
+const states = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas',
+    'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+    'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+    'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
+    'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+]
+
+function populateStates() {
+    const select = document.getElementById('inputState');
+    states.forEach(state => {
+        const option = document.createElement('option');
+        option.value = state;
+        option.text = state;
+        select.appendChild(option);
+    });
+}
+
+
+
+
+function toggleGiraffeInput() {
+    const checkbox = document.getElementById('flexCheckGiraffe');
+    const giraffeInput = document.getElementById('GiraffeInput');
+    const inputField = document.getElementById('numOfGiraffeInput');
+
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            giraffeInput.classList.remove('display-none');
+        } else {
+            giraffeInput.classList.add('display-none');
+            inputField.value = '';
+        }
+    });
+}
+
+function toggleElephantInput() {
+    const checkbox = document.getElementById('flexCheckElephant');
+    const giraffeInput = document.getElementById('ElephantInput');
+    const inputField = document.getElementById('numOfElephantInput');
+
+
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            giraffeInput.classList.remove('display-none');
+        } else {
+            giraffeInput.classList.add('display-none');
+            inputField.value = '';
+        }
+    });
+}
+
+function toggleZebraInput() {
+    const checkbox = document.getElementById('flexCheckZebra');
+    const giraffeInput = document.getElementById('ZebraInput');
+    const inputField = document.getElementById('numOfZebraInput');
+
+
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            giraffeInput.classList.remove('display-none');
+        } else {
+            giraffeInput.classList.add('display-none');
+            inputField.value = '';
+        }
+    });
+}
+
+function toggleSlothInput() {
+    const checkbox = document.getElementById('flexCheckSloth');
+    const giraffeInput = document.getElementById('SlothInput');
+    const inputField = document.getElementById('numOfSlothInput');
+
+
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            giraffeInput.classList.remove('display-none');
+        } else {
+            giraffeInput.classList.add('display-none');
+            inputField.value = '';
+        }
+    });
+}
+
+function toggleRacoonInput() {
+    const checkbox = document.getElementById('flexCheckRacoon');
+    const giraffeInput = document.getElementById('RacoonInput');
+    const inputField = document.getElementById('numOfRacoonInput');
+
+
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            giraffeInput.classList.remove('display-none');
+        } else {
+            giraffeInput.classList.add('display-none');
+            inputField.value = '';
+        }
+    });
+}
+
+
+function DisplayTotal() {
+    let cost = 0;
+
+    // Number of each product
+    let GiraffeinputField = parseInt(document.getElementById('numOfGiraffeInput').value) || 0;
+    let ElephantinputField = parseInt(document.getElementById('numOfElephantInput').value) || 0;
+    let ZebrainputField = parseInt(document.getElementById('numOfZebraInput').value) || 0;
+    let SlothinputField = parseInt(document.getElementById('numOfSlothInput').value) || 0;
+    let RacooninputField = parseInt(document.getElementById('numOfRacoonInput').value) || 0;
+
+    // Product checkbox
+    let checkedGiraffe = document.getElementById('flexCheckGiraffe').checked;
+    let checkedElephant = document.getElementById('flexCheckElephant').checked;
+    let checkedZebra = document.getElementById('flexCheckZebra').checked;
+    let checkedSloth = document.getElementById('flexCheckSloth').checked;
+    let checkedRacoon = document.getElementById('flexCheckRacoon').checked;
+
+    // Expedited checkbox
+    let ExpiditedOrder = document.getElementById('ExpeditedSwitchCheck').checked;
+
+    //Price list
+    let priceResult = document.getElementById('price-result');
+
+    if (checkedGiraffe) {
+        cost += 25.00 * GiraffeinputField;
+    }
+
+    if (checkedElephant) {
+        cost += 25.00 * ElephantinputField;
+    }
+
+    if (checkedZebra) {
+        cost += 25.00 * ZebrainputField;
+    }
+
+    if (checkedSloth) {
+        cost += 25.00 * SlothinputField;
+    }
+
+    if (checkedRacoon) {
+        cost += 40.00 * RacooninputField;
+    }
+
+    if (ExpiditedOrder) {
+        cost += 20;
+    }
+
+    let taxes = 0.25 * cost;
+    let total = taxes + cost;
+
+    document.getElementById('price-value').textContent = "$" + cost.toFixed(2);
+    document.getElementById('taxes-value').textContent = "$" + taxes.toFixed(2);
+    document.getElementById('total-value').textContent = "$" + total.toFixed(2);
+
+
+
+    priceResult.classList.remove('display-none');
 }
